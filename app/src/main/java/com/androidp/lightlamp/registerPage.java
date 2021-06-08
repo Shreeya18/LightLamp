d354a654ecb50621a586fb94e0345fafc5427c0d
Shreeya Dhole <dholeshreeya18@gmail.com>
2021-06-08 18:45:31 +0530

    style changes and on click listener added to registration page

--- app/src/main/java/com/androidp/lightlamp/registerPage.java
+++ app/src/main/java/com/androidp/lightlamp/registerPage.java
@@ -2,13 +2,31 @@ package com.androidp.lightlamp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class registerPage extends AppCompatActivity {
+    private TextView rlog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+
+        rlog = findViewById(R.id.rlog);
+
+        rlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registerPage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }

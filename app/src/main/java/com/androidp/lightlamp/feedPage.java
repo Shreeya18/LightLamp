acfdcb435460f03c0fc0a998ece2e6e665efdab5
Shreeya Dhole <dholeshreeya18@gmail.com>
2021-06-11 22:28:17 +0530

    Created Recycler View

--- app/src/main/java/com/androidp/lightlamp/feedPage.java
+++ app/src/main/java/com/androidp/lightlamp/feedPage.java
@@ -1,14 +1,31 @@
 package com.androidp.lightlamp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class feedPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed_page);
+        setContentView(R.layout.activity_feed_page3);
+
+           RecyclerView review = findViewById(R.id.review);
+           review.setLayoutManager(new LinearLayoutManager(this));
+
+           String[] languages = {"Java","Python", "PHP","C++","C","HTML","CSS"};
+           review.setAdapter(new Radapter(languages));
+
+
+
     }
 }
+
+
+
